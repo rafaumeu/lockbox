76429ff66fb7673d5f2e10117adda4a3ec465615
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-01-19 00:49:00 -0300

    chore(infra): migrate to composer autoloading
    - Initialize composer.json with PSR-4 autoloading
    
    - Switch public/index.php to use vendor/autoload.php
    
    - Remove manual spl_autoload_register

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
-require "../Core/functions.php";
-spl_autoload_register(function ($class) {
-  $class = str_replace("\\", DIRECTORY_SEPARATOR, subject: $class);
-  require base_path("{$class}.php");
-});
-require "../App/Models/Usuario.php";
+require "../vendor/autoload.php";
 session_start();
 
 require base_path('config/routes.php');
